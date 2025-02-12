6fcd362a717cfc5f15e8812b8452bf7231cc077c
Muhammad Umair <140939041+Umairishuman@users.noreply.github.com>
2025-02-12 18:16:27 +0500

    play vs computer is just insane leaving maybe i will come back in two years

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,40 @@
-function StartGame(){
-    
+let game = {
+    board: ["", "", "", "", "", "", "", "", ""],
+    turn : 0,
+    player1: "X",
+    player2: "O",
+    isOn: false
+
 }
+let menu = {
+    isOn: false,
+    selection: 0,
+    menu : document.getElementById("menu"),
+    StartGame: function(){
+        let body = document.querySelector("body");
+        body.style.height = "100vh";
+
+
+        this.menu.style.transition = 'opacity 0.5s ease, transform 0.5s ease'; 
+        this.menu.style.opacity = 0; 
+        this.menu.style.transform = 'scale(0.9)'; 
+
+        this.turnOn();
+        this.setCurrentSelection();
+    },
+    turnOn(){
+        setTimeout(() => { 
+            this.menu.style.opacity = 1; // Fade in
+            this.menu.style.transform = 'scale(1)'; // Zoom in
+        }, 10); 
+
+        this.menu.style.display = 'flex';
+        this.isOn = true;
+    },
+    setCurrentSelection: function(){
+        let playButtons = document.querySelectorAll('#menu button');
+        playButtons[this.selection].style.backgroundColor 
+    }
+}
+
+menu.StartGame();
